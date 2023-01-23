629caedccb155af2018ba09264e62d3f7660bf34
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2023-01-23 17:30:37 +0900

    Modify C# documentation

--- Runtime/DisposableBase.cs
+++ Runtime/DisposableBase.cs
@@ -3,14 +3,29 @@
 namespace Extreal.Core
 {
     /// <summary>
-    /// Base class for classes with unmanaged resources.
+    /// <para>
+    ///   Base class for classes with unmanaged resources.
+    /// </para>
+    /// <para>
+    ///   The following processing is provided in the base class to ensure that the Dispose method is safe.
+    ///   <list type="bullet">
+    ///     <item>Even if the Dispose method is called multiple times, it is executed only once.</item>
+    ///     <item>Call the Dispose method in the Finalizer.</item>
+    ///     <item>Suppress the finalization of this instance.</item>
+    ///   </list>
+    /// </para>
+    /// <para>
+    ///   Inherit this Base class and implement resource release processing in the FreeResources method.
+    ///   Implement the FreeResources method so that no exceptions are raised all resources are released.
+    /// </para>
     /// </summary>
     public abstract class DisposableBase : IDisposable
     {
         private bool isDisposed;
 
         /// <summary>
-        /// Releases unmanaged resources and disposes managed resources.
+        /// <para>Releases unmanaged resources and disposes managed resources.</para>
+        /// <para>Implement so that no exceptions are raised and all resources are released.</para>
         /// </summary>
         protected abstract void FreeResources();
 
