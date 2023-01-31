b58745c8569d66d79319c02f8ac234e8d8597467
Kiyohito Itoh <ito.kiyohito@tis.co.jp>
2023-01-31 15:19:51 +0900

    IDE0053対応でリファクタリング (#3)

--- Tests/Runtime/System/SafeDisposerTest.cs
+++ Tests/Runtime/System/SafeDisposerTest.cs
@@ -7,19 +7,13 @@ namespace Extreal.Core.Common.System.Test
     {
         [Test]
         public void TargetIsNull() =>
-            Assert.That(() =>
-                {
-                    _ = new SafeDisposer(null, () => { }, () => { });
-                },
+            Assert.That(() => _ = new SafeDisposer(null, () => { }, () => { }),
                 Throws.TypeOf<ArgumentNullException>().With.Message.
                     EqualTo($"Value cannot be null.{Environment.NewLine}Parameter name: target"));
 
         [Test]
         public void BothReleaseManagedResourcesAndReleaseUnmanagedResourcesAreNull() =>
-            Assert.That(() =>
-                {
-                    _ = new SafeDisposer(new object(), null, null);
-                },
+            Assert.That(() => _ = new SafeDisposer(new object(), null, null),
                 Throws.TypeOf<ArgumentException>().With.Message
                     .EqualTo("Either releaseManagedResources or releaseUnmanagedResources is required"));
     }
