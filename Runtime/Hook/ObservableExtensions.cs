15ea29488d502c616fbb4db6e36a4a4c7a2407be
Kiyohito Itoh <ito.kiyohito@tis.co.jp>
2023-06-13 09:45:38 +0900

    IObservableのSubscribeをフックできるようにする (#6)

---
+++ Runtime/Hook/ObservableExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Extreal.Core.Logging;
+using UniRx;
+
+namespace Extreal.Core.Common.Hook
+{
+    /// <summary>
+    /// Class for extending Observable.
+    /// </summary>
+    public static class ObservableExtensions
+    {
+        private static readonly ELogger Logger = LoggingManager.GetLogger(nameof(ObservableExtensions));
+
+        /// <summary>
+        /// Hooks a notification.
+        /// </summary>
+        /// <remarks>
+        /// The hook must be implemented in such a way that no exception is raised.
+        /// If an exception is raised by the hook, nothing is done because it does not affect the processing of other Subscribers.
+        /// </remarks>
+        /// <param name="source">Observable</param>
+        /// <param name="hook">Processing for hook</param>
+        /// <typeparam name="T">Type of value to be notified</typeparam>
+        /// <returns>Observable</returns>
+        /// <exception cref="ArgumentNullException">If hook is null</exception>
+        public static IDisposable Hook<T>(this IObservable<T> source, Action<T> hook)
+        {
+            if (hook == null)
+            {
+                throw new ArgumentNullException(nameof(hook));
+            }
+
+            return source.Subscribe(value => InvokeHookSafely(hook, value));
+        }
+
+        private static void InvokeHookSafely<T>(Action<T> hook, T value)
+        {
+            try
+            {
+                hook.Invoke(value);
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsDebug())
+                {
+                    // Outputs the log with Error level so that the developer is aware of the error during development.
+                    Logger.LogError("Error has occurred on hook", e);
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Runtime/Hook/ObservableExtensionsTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Extreal.Core.Logging;
+using NUnit.Framework;
+using UniRx;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Extreal.Core.Common.Hook.Test
+{
+    public class ObservableExtensionsTest
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp() => LoggingManager.Initialize(logLevel: LogLevel.Debug);
+
+        private CompositeDisposable disposables;
+
+        [SetUp]
+        public void SetUp() => disposables = new CompositeDisposable();
+
+        [TearDown]
+        public void TearDown() => disposables.Dispose();
+
+        [Test]
+        public void HookIsNull() =>
+            Assert.That(() =>
+                {
+                    var point = new ReactiveProperty<int>(0).AddTo(disposables);
+                    point.Hook(null);
+                },
+                Throws.TypeOf<ArgumentNullException>().With.Message.Contain("Parameter name: hook"));
+
+        [Test]
+        public void Hook()
+        {
+            var total = 0;
+
+            var point = new ReactiveProperty<int>(0).AddTo(disposables);
+            Action<int> hook = value => total += value;
+            point.Hook(hook).AddTo(disposables);
+
+            point.Value = 1;
+            point.Value = 2;
+            point.Value = 3;
+
+            Assert.That(total, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void ExceptionOccurredOnHook()
+        {
+            LogAssert.ignoreFailingMessages = true;
+            LogAssert.Expect(LogType.Error, new Regex(".*Error has occurred on hook.*"));
+
+            var total = 0;
+
+            var point = new ReactiveProperty<int>(0).AddTo(disposables);
+            Action<int> onNext = value => total += value;
+            Action<int> hook = _ => throw new Exception("HOOK ERROR");
+            point.Subscribe(onNext).AddTo(disposables);
+            point.Hook(hook).AddTo(disposables);
+
+            point.Value = 1;
+            point.Value = 2;
+            point.Value = 3;
+
+            Assert.That(total, Is.EqualTo(6));
+        }
+    }
+}
